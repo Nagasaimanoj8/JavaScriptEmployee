4e87e75824f6679035a63950a8f5d9b9a8446a73
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-15 10:29:10 +0530

    [Manoj]Adding_Attendence

---
+++ EmployeeWage.js
@@ -0,0 +1,7 @@
+////////////////////////UC1////////////////
+let empInput = Math.floor(Math.random() * 10) % 2;
+if (empInput == 1) {
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is absent");
+}

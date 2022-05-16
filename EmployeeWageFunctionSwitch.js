fe6376f3020b977b498215924771ef45903a8ef3
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-16 20:15:57 +0530

    [Manoj]Adding_UsingCondition

--- EmployeeWageFunctionSwitch.js
+++ EmployeeWageFunctionSwitch.js
@@ -4,30 +4,28 @@ const EMP_RATE_PER_HR = 20;
 const MAX_WORKING_Days = 20;
 let empHrs = 0;
 let totalEmpHrs = 0;
-for (day = 1; day < MAX_WORKING_Days; day++) {
-    let empInput = Math.floor(Math.random() * 10) % 3;
 
-    function getWorkingHrs(empInput) {
-        switch (empInput) {
-            case FULL_TIME:
-                console.log('Full_Time employee is present');
-                empHrs = 8;
-                return empHrs;
-            case PART_TIME:
-                console.log('Part_Time Employee is Present');
-                empHrs = 4;
-                return empHrs;
-            default:
-                console.log('employee absent');
-                return empHrs;
-        }
-        empHrs = getWorkingHrs(empInput);
-        totalEmpHrs = empHrs;
+function getWorkingHrs(empInput) {
+    switch (empInput) {
+        case FULL_TIME:
+            console.log('Full_Time employee is present');
+            empHrs = 8;
+            return empHrs;
+        case PART_TIME:
+            console.log('Part_Time Employee is Present');
+            empHrs = 4;
+            return empHrs;
+        default:
+            console.log('employee absent');
+            return empHrs;
     }
+    empHrs = getWorkingHrs(empInput);
+    totalEmpHrs = empHrs;
+
     empHrs = getWorkingHrs(empInput);
     totalEmpHrs += empHrs;
 }
-const MAX_WORKING_HOURS = 160;
+const MAX_WORKING_HOURS = 100;
 let totalWorkingDays = 0;
 let totalWorkingHours = 0;
 let totalWage = 0;
@@ -35,6 +33,7 @@ while (totalWorkingDays <= MAX_WORKING_Days && totalWorkingHours <= MAX_WORKING_
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalWorkingHours = getWorkingHrs(empCheck);
     totalWorkingDays++;
+    totalWorkingHours += empHrs;
     totalWage += totalWorkingHours * EMP_RATE_PER_HR;
 }
 console.log("UC5: Total wage is " + totalWage);

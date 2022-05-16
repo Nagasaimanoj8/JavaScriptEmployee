19b6c9996f8de86d4f93fc3bef928995ae80eeb8
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-16 19:45:43 +0530

    [Manoj]Adding_UsingForLoop

--- EmployeeWageFunctionSwitch.js
+++ EmployeeWageFunctionSwitch.js
@@ -3,24 +3,32 @@ const PART_TIME = 2;
 const EMP_RATE_PER_HR = 20;
 const MAX_WORKING_Days = 20;
 let empHrs = 0;
-let empInput = Math.floor(Math.random() * 10) % 3;
+let totalEmpHrs = 0;
+for (day = 1; day < MAX_WORKING_Days; day++) {
+    let empInput = Math.floor(Math.random() * 10) % 3;
 
-function getWorkingHrs(empInput) {
-    switch (empInput) {
-        case FULL_TIME:
-            console.log('Full_Time employee is present');
-            empHrs = 8;
-            return empHrs;
-        case PART_TIME:
-            console.log('Part_Time Employee is Present');
-            empHrs = 4;
-            return empHrs;
-        default:
-            console.log('employee absent');
-            return empHrs;
+    function getWorkingHrs(empInput) {
+        switch (empInput) {
+            case FULL_TIME:
+                console.log('Full_Time employee is present');
+                empHrs = 8;
+                return empHrs;
+            case PART_TIME:
+                console.log('Part_Time Employee is Present');
+                empHrs = 4;
+                return empHrs;
+            default:
+                console.log('employee absent');
+                return empHrs;
+        }
+        empHrs = getWorkingHrs(empInput);
+        totalEmpHrs = empHrs;
     }
+    empHrs = getWorkingHrs(empInput);
+    totalEmpHrs += empHrs;
 }
-empHrs = getWorkingHrs(empInput);
-empWage = EMP_RATE_PER_HR * empHrs;
+
+empWage = EMP_RATE_PER_HR * totalEmpHrs;
 console.log("UC2 empWage" + empWage);
 console.log("UC3 functions:Getting emphrs" + empWage)
+console.log("UC4 forloop:TotalWageFor" + MAX_WORKING_Days + "days" + empWage);
